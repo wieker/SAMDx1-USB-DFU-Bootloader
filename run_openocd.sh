3587e4fb5dd10e3ce2df3e057c423e68a08c9afd
kabramovich <wieker@gmail.com>
2020-05-17 02:36:07 +0300

    started OpenOCD config

--- run_openocd.sh
+++ run_openocd.sh
@@ -1,1 +1,4 @@
-Projects/linux/hackrf/openocd-bin/bin/openocd -f Projects/linux/hackrf/openocd-bin/share/openocd/scripts/interface/cmsis-dap.cfg -f Projects/linux/hackrf/openocd-bin/share/openocd/scripts/target/at91samdXX.cfg 
+~/Projects/linux/hackrf/openocd-bin/bin/openocd \
+	-f ~/Projects/linux/hackrf/openocd-bin/share/openocd/scripts/interface/cmsis-dap.cfg \
+	-f ~/Projects/linux/hackrf/openocd-bin/share/openocd/scripts/target/at91samdXX.cfg \
+	-c "program example-apps/vcp/make/build/vcp.elf verify; exit"
